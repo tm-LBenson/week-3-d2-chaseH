5db8581e23e150a919fd86e0a7a9c8d70e785275
Chasity Hampton <rainbowunicorn561@gmail.com>
2026-02-17 19:34:46 -0600

    learning functions / calling functions

---
+++ app.js
@@ -0,0 +1,59 @@
+"use strict";
+
+// // var
+
+// const user1Name = "Ada";
+// let user1Score = 10;
+
+// const user2Name = "Grace";
+// let user2Score = 20;
+
+const user1 = {
+    userName: 'Ada',
+    userScore: 10,
+};
+
+const user2 = {
+    userName: "Grace",
+    userScore: 20,
+}
+
+const metaPhone4 = {
+    screensize: 1.9,
+    processor: "4.5gz",
+}
+    powerOn: function(){
+        // send signal to battery
+    }
+
+    const header = {
+        position: "sticky",
+        width: "100%",
+        links: ["Home","About","Sales"],
+    }
+
+    getCodeSpaces: function (){
+        // go to DB and find out 
+    }
+
+console.log(user1.userName + "" + user1.userScore);
+console.log(user2.userName, user2.userScore);
+
+user1.userScore = user1.userScore + 5;
+user1.userScore = user1.userScore + 5;
+user1.userScore = user1.userScore + 5;
+
+
+
+console.log(user1.userName, user1.userScore);
+
+
+function displayText(){
+  document.writeln("Hello!")
+  console.log("Hi!!!")
+  console.table(["Hello","Hi"])
+}
+
+displayText()
+
+
---
+++ script.js
@@ -0,0 +1,3 @@
+"use strict";
+
+function makeUser(name, score){}
